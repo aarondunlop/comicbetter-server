6faf6f73fc5c332f97b24b16f9e9a1f5a30af8e8
Aaron Dunlop <aarondunlop@users.noreply.github.com>
2017-10-28 18:05:44 -0500

    Adding logic for logging.

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ from logging.handlers import RotatingFileHandler
 if __name__ == '__main__':
     formatter = logging.Formatter(
         "[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s")
-    handler = RotatingFileHandler('var/log/server.log', maxBytes=10000000, backupCount=5)
+    handler = RotatingFileHandler('/var/log/server.log', maxBytes=10000000, backupCount=5)
     handler.setLevel(logging.DEBUG)
     handler.setFormatter(formatter)
     app.logger.setLevel(logging.DEBUG)
