352ca8bbb73aed17264d79c038fa471e6d9a3f37
Aaron Dunlop <aarondunlop@users.noreply.github.com>
2017-10-28 17:56:35 -0500

    Adding logging info for Debug mode.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 # Import flask and template operators
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import logging
 
 # Import SQLAlchemy
@@ -46,14 +46,14 @@ def handle_bad_request(e):
     app.logger.info('Bad request', e)
     return flask.make_response('bad request', 400)
 
-if app.debug is not True:
-    import logging
-    from logging.handlers import RotatingFileHandler
-    file_handler = RotatingFileHandler('var/log/server.log', maxBytes=1024 * 1024 * 100, backupCount=20)
-    file_handler.setLevel(logging.ERROR)
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-    file_handler.setFormatter(formatter)
-    app.logger.addHandler(file_handler)
+#if app.debug is not True:
+#    import logging
+#    from logging.handlers import RotatingFileHandler
+#    file_handler = RotatingFileHandler('var/log/server.log', maxBytes=1024 * 1024 * 100, backupCount=20)
+#    file_handler.setLevel(logging.ERROR)
+#    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+#    file_handler.setFormatter(formatter)
+#    app.logger.addHandler(file_handler)
 
 # Import a module / component using its blueprint handler variable (mod_auth)
 #from app.mod_auth.controllers import mod_auth as auth_module
